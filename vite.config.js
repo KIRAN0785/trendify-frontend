8337eba63e15182b0cd638c6481783e579a0d112
kiran <kiran0785.be21@chitkara.edu.in>
2025-05-09 13:19:29 +0530

    z

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port:5173}
-})
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    allowedHosts: ['cc-frontend-rsp0.onrender.com']
+
+  }
+});
